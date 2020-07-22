7eb20391ba96bab3d8d04bae244d0b5338022207
Gideon Anyalewechi <giddy@pop-os.localdomain>
2020-07-22 13:36:49 +0100

    A few outlook changes

--- pacent_tech_2020/lesson1.py
+++ pacent_tech_2020/lesson1.py
@@ -5,12 +5,12 @@ HELLO WORLD! VARIABLE DECLARATION AND THE STD_OUT
 
 Here is a sample line of code that can be executed in Python:
 
-> print("Hello, World!")
+>>> print("Hello, World!")
 
 You can just as easily store a string as a variable and then print it to stdout:
 
-> my_string = "Hello, World!"
-> print(my_string)
+>>> my_string = "Hello, World!"
+>>> print(my_string)
 
 The above code will print Hello, World! on your screen. Try it yourself in the editor below!
 
@@ -30,8 +30,8 @@ Users can be allowed to provide the data for you program using the through the '
 
 Sample code :
 
-> name = input('Enter your name:\n')
-> print('Hello '+name+'!')
+>>> name = input('Enter your name:\n')
+>>> print('Hello '+name+'!')
 
 Sample input:
 
@@ -52,30 +52,30 @@ WORKING WITH STRINGS
 "The language 'Python' is named after Monty Python, not the snake."
 "One of Python's strengths is its diverse and supportive community."
 
-name = "ada lovelace"
-print(name.title())
-print(name.upper())
-print(name.lower())
+>>> name = "ada lovelace"
+>>> print(name.title())
+>>> print(name.upper())
+>>> print(name.lower())
 
 A method is an action that Python can perform on a piece of data
 
-full_name = "{} {}".format(first_name, last_name)
+>>> full_name = "{} {}".format(first_name, last_name)
 
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
-print(full_name)
+>>> first_name = "ada"
+>>> last_name = "lovelace"
+>>> full_name = f"{first_name} {last_name}"
+>>> print(full_name)
 
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
-print(f"Hello, {full_name.title()}!")
+>>> first_name = "ada"
+>>> last_name = "lovelace"
+>>> full_name = f"{first_name} {last_name}"
+>>> print(f"Hello, {full_name.title()}!")
 
 Adding and stripping whitespaces
 
 RESEARCH - Escape sequences
 
-favorite_language = 'python '
-favorite_language.rstrip()
+>>> favorite_language = 'python '
+>>> favorite_language.rstrip()
 
 '''
