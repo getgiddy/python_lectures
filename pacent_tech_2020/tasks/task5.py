ed653333a0636e816e1e32ebce948ca1668ab13b
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2020-08-05 18:51:58 +0100

    Task 5

---
+++ pacent_tech_2020/tasks/task5.py
@@ -0,0 +1,34 @@
+'''
+
+############################################################################################
+
+TASK 5
+
+> Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+
+>>> car = 'subaru'
+>>> print("Is car == 'subaru'? I predict True.")
+>>> print(car == 'subaru')
+>>> print("\nIs car == 'audi'? I predict False.")
+>>> print(car == 'audi')
+
+• Look closely at your results, and make sure you understand why each line
+evaluates to True or False .
+• Create at least ten tests. Have at least five tests evaluate to True and
+another five tests evaluate to False .
+
+> More Conditional Tests: You don’t have to limit the number of tests you
+create to ten. If you want to try more comparisons, write more tests and add
+them to conditional_tests.py. Have at least one True and one False result for
+each of the following:
+• Tests for equality and inequality with strings
+• Tests using the lower() method
+• Numerical tests involving equality and inequality, greater than and
+less than, greater than or equal to, and less than or equal to
+• Tests using the 'and' keyword and the 'or' keyword
+• Test whether an item is in a list (Hint: you can do a little research on the 'in' keyword in python)
+• Test whether an item is not in a list
+
+'''
